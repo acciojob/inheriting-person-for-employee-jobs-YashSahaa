c31a01aadfb8cc5a2b1e58877f816712f344f080
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-30 23:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function Employee(name, age, jobTitle) {
 	Person.call(this,name,age);
 	this.jobTitle = jobTitle;
 }
+Employee.prototype.greet = function() {
+    Person.prototype.greet.call(this);
+}
 Employee.prototype.jobGreet = function () {
 	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
 }
