b8b72f0fa1bc05a9a01011f10db93e1477d81a97
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-30 23:41:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 	this.age = age;
 }
 Person.prototype.greet = function () {
-	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old.")
+	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.")
 }
 function Employee(name, age, jobTitle) {
 	Person.call(this,name,age);
@@ -14,7 +14,7 @@ Employee.prototype.greet = function() {
     Person.prototype.greet.call(this);
 }
 Employee.prototype.jobGreet = function () {
-	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
+	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
 }
 Object.setPrototypeOf(Employee.prototype,Person.prototype);
 // Do not change code below this line
