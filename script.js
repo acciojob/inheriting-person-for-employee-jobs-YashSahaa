129b05efa110b0fd89189445842c0951ea361d41
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-03 21:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Person {
 
   greet() {
     // Return greeting
-	  console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.");
+	  console.log("Hello, my name is "+this.name+"and I am "+this.age+" years old.");
   }
 }
 
