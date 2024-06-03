36f053a184a94405cedf48c8a54ef4038633f42a
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-03 21:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 // complete this js code
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-Person.prototype.greet = function () {
-	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.")
-}
-function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
-}
-// Employee.prototype.greet = function() {
-//     Person.prototype.greet.call(this);
-// }
-Employee.prototype = Object.create(Person.prototype);
+class Person {
+  constructor(name, age) {
+    // Initialize properties
+	  this.name = name;
+ 	  this.age = age;
+  }
 
-Employee.prototype.jobGreet = function () {
-	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
+  greet() {
+    // Return greeting
+	  console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.");
+  }
 }
-	//Object.setPrototypeOf(Person.prototype,Employee.prototype);
-// Do not change code below this line
+
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    // Call the Person constructor
+	  super(name,age);
+    // Initialize jobTitle
+	  this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    // Return job greeting
+	  console.log("Hello, my name is "+this.name+" I am "+this.age+" years old, and my job title is "+this.jobTitle+".");
+  }
+}
+
+
+// function Person(name, age) {
+// 	this.name = name;
+// 	this.age = age;
+// }
+// Person.prototype.greet = function () {
+// 	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old.")
+// }
+// function Employee(name, age, jobTitle) {
+// 	Person.call(this,name,age);
+// 	this.jobTitle = jobTitle;
+// }
+// // Employee.prototype.greet = function() {
+// //     Person.prototype.greet.call(this);
+// // }
+// Employee.prototype = Object.create(Person.prototype);
+
+// Employee.prototype.jobGreet = function () {
+// 	console.log("Hello, my name is "+this.name+" I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
+// }
+// 	//Object.setPrototypeOf(Person.prototype,Employee.prototype);
+// // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
 
-const person = new Person("Alice",25);
-person.greet();
-const employee = new Employee("Bob",30,"mana");
-employee.greet();
-employee.jobGreet();
+// const person = new Person("Alice",25);
+// person.greet();
+// const employee = new Employee("Bob",30,"mana");
+// employee.greet();
+// employee.jobGreet();
